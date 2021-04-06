e4a9802b9fd840d34a60d4633eb2effb6ef22fdc
Calvin Wong <ctwong958@berkeley.edu>
2021-04-05 20:04:09 -0700

    Small visual tweaks i forgot to commit

--- DeMythyfi_App/Scene/BG_Blurred.gd
+++ DeMythyfi_App/Scene/BG_Blurred.gd
@@ -3,11 +3,14 @@ extends TextureRect
 var blur_amount = 0
 var blur_change : float = .01
 
-func _physics_process(delta):
-	
-	blur_amount = clamp(blur_amount + blur_change, 2.0, 5.0)
-#	print("b", blur_amount)
-	material.set_shader_param("blur_amount", blur_amount)
+func _ready():
+	material.set_shader_param("blur_amount", randf()*3 + 1.5)
+
+#func _physics_process(delta):
+#
+#	blur_amount = clamp(blur_amount + blur_change, 2.0, 5.0)
+##	print("b", blur_amount)
+#	material.set_shader_param("blur_amount", blur_amount)
 
 
 func _on_Timer_timeout():
