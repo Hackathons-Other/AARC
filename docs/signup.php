3b2650ed78fe1fd1d4859be6841966ad737f9cc7
Calvin Wong <ctwong958@berkeley.edu>
2021-05-03 13:45:31 -0700

    Notes, suggestions

--- docs/signup.php
+++ docs/signup.php
@@ -1,7 +1,27 @@
+// <?php
+//     if (isset($_POST['submit'])) {
+//         $email = $_POST['email'];
+//         echo "<p> " . $email . "</p>”;
+//         echo "<p> HelloSDFSDF world </p>";
+//     }
+// ?>
+
 <?php
-    if (isset($_POST['submit'])) {
-        $email = $_POST['email'];
-        echo "<p> " . $email . "</p>”;
-        echo "<p> HelloSDFSDF world </p>";
-    }
-?>
+
+$errors = [];
+$data = [];
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
